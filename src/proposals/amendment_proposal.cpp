d2b280ce9c5e110bfe682d010f8f58c484877166
tlacloc <desneruda@gmail.com>
2023-02-10 12:37:54 -0600

    update format

--- src/proposals/amendment_proposal.cpp
+++ src/proposals/amendment_proposal.cpp
@@ -1,6 +1,6 @@
 #include <proposals/amendment_proposal.hpp>
 
-void AmendmentProposal::create_impl (std::map<std::string, common::types::variant_value> & args)
+void AmendmentProposal::create_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -17,13 +17,11 @@ void AmendmentProposal::create_impl (std::map<std::string, common::types::varian
 
   eosio::check(ppitr->awaiting.size() == 0, "This proposal is awaiting for other proposal to pass!");
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.special_attributes.insert(std::make_pair("budget", util::get_attr<eosio::asset>(args, "budget")));
-  });
-
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.special_attributes.insert(std::make_pair("budget", util::get_attr<eosio::asset>(args, "budget"))); });
 }
 
-void AmendmentProposal::update_impl (std::map<std::string, common::types::variant_value> & args)
+void AmendmentProposal::update_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -36,13 +34,11 @@ void AmendmentProposal::update_impl (std::map<std::string, common::types::varian
 
   eosio::check(ppitr->status == common::proposals::phase_debate, "Amendment proposal can be only created when main proposal is on debate phase");
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.special_attributes.at("budget") = util::get_attr<eosio::asset>(args, "budget", pitr->special_attributes.at("budget"));
-  });
-
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.special_attributes.at("budget") = util::get_attr<eosio::asset>(args, "budget", pitr->special_attributes.at("budget")); });
 }
 
-void AmendmentProposal::cancel_impl (std::map<std::string, common::types::variant_value> & args)
+void AmendmentProposal::cancel_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -53,8 +49,6 @@ void AmendmentProposal::cancel_impl (std::map<std::string, common::types::varian
 
   eosio::check(ppitr != proposals_t.end(), "Proposal proposal must have an existing proposal parent");
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.status = common::proposals::status_rejected;
-  });
-
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.status = common::proposals::status_rejected; });
 }
