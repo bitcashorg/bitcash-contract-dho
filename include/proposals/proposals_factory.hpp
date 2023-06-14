09f2089902f28e152f0c9a73b530d37df19c2c91
Erick Casanova <desneruda@gmail.com>
2023-06-14 09:25:53 -0600

    🚀 feat(proposals_factory.hpp): add support for ChangeTimeProposal The ProposalsFactory class now supports the creation of a new ChangeTimeProposal object. This is achieved by adding a new case statement to the switch statement in the Factory method. The new ChangeTimeProposal object is created and returned when the type of proposal is "change_time". This change allows the application to handle a new type of proposal, which is the change of the debate time.

--- include/proposals/proposals_factory.hpp
+++ include/proposals/proposals_factory.hpp
@@ -4,35 +4,38 @@
 #include "amendment_proposal.hpp"
 #include "extend_debate_proposal.hpp"
 #include "shorten_debate_proposal.hpp"
+#include "change_time_proposal.hpp"
 
 #include "../common/constants.hpp"
 
-class ProposalsFactory {
+class ProposalsFactory
+{
 
-  public:
+public:
+  static Proposal *Factory(proposals &_contract, const eosio::name &type)
+  {
+    switch (type.value)
+    {
+    case common::proposals::type_main.value:
+      return new MainProposal(_contract);
 
-    static Proposal * Factory(proposals & _contract, const eosio::name & type) {
-      switch (type.value)
-      {
-        case common::proposals::type_main.value:
-          return new MainProposal(_contract);
+    case common::proposals::type_amendment.value:
+      return new AmendmentProposal(_contract);
 
-        case common::proposals::type_amendment.value:
-          return new AmendmentProposal(_contract);
+    case common::proposals::type_extend_debate.value:
+      return new ExtendDebateProposal(_contract);
 
-        case common::proposals::type_extend_debate.value:
-          return new ExtendDebateProposal(_contract);
+    case common::proposals::type_shorten_debate.value:
+      return new ShortenDebateProposal(_contract);
 
-        case common::proposals::type_shorten_debate.value:
-          return new ShortenDebateProposal(_contract);
+    case common::proposals::type_change_time.value:
+      return new ChangeTimeProposal(_contract);
 
-        default:
-          break;
-      }
-
-      eosio::check(false, "Unknown proposal type " + type.to_string());
-      return nullptr;
+    default:
+      break;
     }
 
+    eosio::check(false, "Unknown proposal type " + type.to_string());
+    return nullptr;
+  }
 };
-
