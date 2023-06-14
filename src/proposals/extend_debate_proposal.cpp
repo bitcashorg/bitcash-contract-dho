69ecd2ad67a603862bf2a3ef9be3eed4dfe90223
Erick Casanova <desneruda@gmail.com>
2023-06-14 09:26:15 -0600

    🔨 refactor(extend_debate_proposal.cpp): remove unnecessary spaces in function parameters The changes remove unnecessary spaces in function parameters to improve code readability.

--- src/proposals/extend_debate_proposal.cpp
+++ src/proposals/extend_debate_proposal.cpp
@@ -1,6 +1,6 @@
 #include <proposals/extend_debate_proposal.hpp>
 
-void ExtendDebateProposal::create_impl (std::map<std::string, common::types::variant_value> & args)
+void ExtendDebateProposal::create_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -19,13 +19,11 @@ void ExtendDebateProposal::create_impl (std::map<std::string, common::types::var
 
   // eosio::check(util::get_attr<int64_t>(args, "days", pitr->special_attributes.at("days")) < 5, 'Proposal can not be extended more than 5 days');
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.special_attributes.insert(std::make_pair("days", util::get_attr<int64_t>(args, "days")));
-  });
-
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.special_attributes.insert(std::make_pair("days", util::get_attr<int64_t>(args, "days"))); });
 }
 
-void ExtendDebateProposal::update_impl (std::map<std::string, common::types::variant_value> & args)
+void ExtendDebateProposal::update_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -41,16 +39,14 @@ void ExtendDebateProposal::update_impl (std::map<std::string, common::types::var
   eosio::check(ppitr->awaiting.size() == 0, "This proposal is awaiting for other proposal to pass!");
 
   uint64_t days = util::get_attr<int64_t>(args, "days", pitr->special_attributes.at("days"));
-  eosio::check(days < 5,'Proposal can not be extended more than 5 days');
+  eosio::check(days < 5, 'Proposal can not be extended more than 5 days');
   // eosio::check(util::get_attr<int64_t>(args, "days", pitr->special_attributes.at("days")) < 5, 'Proposal can not be extended more than 5 days');
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.special_attributes.at("days") = util::get_attr<int64_t>(args, "days", pitr->special_attributes.at("days"));
-  });
-
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.special_attributes.at("days") = util::get_attr<int64_t>(args, "days", pitr->special_attributes.at("days")); });
 }
 
-void ExtendDebateProposal::cancel_impl (std::map<std::string, common::types::variant_value> & args)
+void ExtendDebateProposal::cancel_impl(std::map<std::string, common::types::variant_value> &args)
 {
   proposals::proposal_tables proposals_t(contract_name, contract_name.value);
 
@@ -61,12 +57,9 @@ void ExtendDebateProposal::cancel_impl (std::map<std::string, common::types::var
 
   eosio::check(ppitr != proposals_t.end(), "Proposal must have an existing proposal parent");
 
-  proposals_t.modify(pitr, contract_name, [&](auto & item){
-    item.status = common::proposals::status_rejected;
-  });
+  proposals_t.modify(pitr, contract_name, [&](auto &item)
+                     { item.status = common::proposals::status_rejected; });
 
-  proposals_t.modify(ppitr, contract_name, [&](auto & item){
-    item.awaiting.erase(std::remove(item.awaiting.begin(), item.awaiting.end(), proposal_id), item.awaiting.end());
-  });
-
+  proposals_t.modify(ppitr, contract_name, [&](auto &item)
+                     { item.awaiting.erase(std::remove(item.awaiting.begin(), item.awaiting.end(), proposal_id), item.awaiting.end()); });
 }
