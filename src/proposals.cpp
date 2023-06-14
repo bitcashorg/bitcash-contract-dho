3161e5ffee64ea2deaee4e10c2d2a4b050ce2ecb
Erick Casanova <desneruda@gmail.com>
2023-06-14 09:26:31 -0600

    🚀 feat(proposals.cpp): add change_time_proposal.cpp A new proposal type, change_time_proposal.cpp, has been added to the proposals.cpp file. This proposal type allows for the modification of the time allotted for a debate.

--- src/proposals.cpp
+++ src/proposals.cpp
@@ -7,6 +7,7 @@
 #include "proposals/amendment_proposal.cpp"
 #include "proposals/extend_debate_proposal.cpp"
 #include "proposals/shorten_debate_proposal.cpp"
+#include "proposals/change_time_proposal.cpp"
 
 #include "phases/base_phase.cpp"
 #include "phases/dialog_phase.cpp"
